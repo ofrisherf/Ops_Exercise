d7ecf231b91dea31d189f3b20c178f131b744436
Ofri Sherf <ec2-user@ip-172-31-30-102.us-east-2.compute.internal>
2018-08-28 13:05:54 +0000

    added variables, general error handling

--- ops-exercise.py
+++ ops-exercise.py
@@ -5,26 +5,46 @@ def fail_deploy():
     print "Failing deployment....."
     call(["/usr/local/bin/docker-compose","down"])
 
+def print_exception(error):
+    print error 
+    traceback.print_exc()
+
+# Params
+bucket = "https://s3.eu-central-1.amazonaws.com/devops-exercise/pandapics.tar.gz"
+pics_tar = "pandapics.tar.gz"
+images_dir = '/home/ec2-user/repo/Ops_Exercise/public/images/'
+health_url = 'http://localhost:3000/health'
+
+# Imports
 import requests,tarfile,urllib2,time,traceback
 from subprocess import call
 
-# Params
-url='http://localhost:3000/health'
-
 # Download the images file
-url = "https://s3.eu-central-1.amazonaws.com/devops-exercise/pandapics.tar.gz"
-filename = url.split("/")[-1]
-with open(filename, "wb") as f:
-    r = requests.get(url)
-    f.write(r.content)
+try:
+    filename = bucket.split("/")[-1]
+    with open(filename, "wb") as f:
+        r = requests.get(bucket)
+        f.write(r.content)
+except Exception:
+    print_exception("Error getting the images file.")
+    fail_deploy()
 
-# Untar the images archive to /public/images
-tar = tarfile.open("pandapics.tar.gz","r:gz")
-tar.extractall("./public/images")
-tar.close()
+# Untar the images archive to public/images
+try:
+    tar = tarfile.open(pics_tar,"r:gz")
+    tar.extractall(images_dir)
+    tar.close()
+except Exception:
+    print_exception("Error extracting the images to public/images")
+    fail_deploy()
 
 # Run docker-compose up
-call(["/usr/local/bin/docker-compose","up","-d"])
+try:
+    call(["/usr/local/bin/docker-compose","up","-d"])
+except:
+    print_exception("Error: docker-compose up command failed.")
+    fail_deploy()
+
 
 # Give the app a few seconds to start
 print "Starting up the app..."
@@ -32,12 +52,11 @@ time.sleep(5)
 
 # Run the health check with a 5 seconds timeout
 try:
-    r = requests.get(url, timeout=5)
+    r = requests.get(health_url, timeout=5)
     health = r.status_code
     if health != 200:
        raise ValueError("Health check returned: " + str(health))
 except Exception:
-    print "Exception caught:"
-    traceback.print_exc()
-    fail_deploy()
+       print_exception("Health check failed.")
+       fail_deploy()
 
