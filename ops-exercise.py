a6aeb706fc05c1c745dd0488db41a831193def20
Ofri Sherf <ec2-user@ip-172-31-30-102.us-east-2.compute.internal>
2018-08-28 11:39:03 +0000

    Changed db container+localhost

--- ops-exercise.py
+++ ops-exercise.py
@@ -14,24 +14,10 @@ tar = tarfile.open("pandapics.tar.gz","r:gz")
 tar.extractall("./public/images")
 tar.close()
 
-# Render the docker-compose.yml template
-env = Environment(loader=FileSystemLoader('templates'))
-template = env.get_template('template.yml')
-
-# Get the insntance IP address from AWS metadata
-res = requests.get('http://169.254.169.254/latest/meta-data/public-ipv4')
-ipaddr = res.text
-output_from_parsed_template = template.render(ip=ipaddr)
-print output_from_parsed_template
-
-# Save the results to a docker-compose.yml file
-with open("docker-compose.yml", "wb") as fh:
-    fh.write(output_from_parsed_template)
-
 # Run docker-compose up
 call(["/usr/local/bin/docker-compose","up","-d"])
 
-url='http://' + ipaddr + ':3000/health'
+url='http://localhost:3000/health'
 
 # Give the app a few seconds to start
 print "Starting up the app..."
@@ -48,5 +34,4 @@ except Exception:
     traceback.print_exc()
     print "Failing deployment....."
     call(["/usr/local/bin/docker-compose","down"])
-    pass
 
